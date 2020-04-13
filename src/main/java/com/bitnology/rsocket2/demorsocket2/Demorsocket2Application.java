386deb2f0546b52d370119208475ecb77fd0d20a
klhaim7 <klhaim7@gmail.com>
2020-04-13 21:12:37 +0300

    awcond commit

---
+++ src/main/java/com/bitnology/rsocket2/demorsocket2/Demorsocket2Application.java
@@ -0,0 +1,13 @@
+package com.bitnology.rsocket2.demorsocket2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demorsocket2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demorsocket2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bitnology/rsocket2/demorsocket2/Demorsocket2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitnology.rsocket2.demorsocket2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demorsocket2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
